f67d306d7ca7b450b59825b8cab7515a4354aaf8
Antonio <ajpelu@gmail.com>
2017-10-04 11:19:27 +0200

    update app

--- inst/shiny-examples/respineapp/server.R
+++ inst/shiny-examples/respineapp/server.R
@@ -12,8 +12,8 @@ library('rgeos')
 # Crear landscape vacío
 set.seed(123)
 
-ancho <- 63 * 2
-alto <- 53 * 2
+ancho <- 53 * 2
+alto <- 63 * 2
 
 m <- matrix(nrow=alto, ncol=ancho, byrow = T)
 r <- raster(m)
@@ -23,30 +23,31 @@ r[] <- 0
 source('createLandscape.R', local=TRUE)
 source('initRichness.R', local = TRUE)
 source('dist2nf.R', local = TRUE)
-
+source('disper.R', local=TRUE)
+source('disper_time.R', local=TRUE)
 
 shinyServer(
   function(input, output, session){
 
+    vb <- reactiveValues(doPlot = FALSE)
+
+    observeEvent(input$doPaisaje, {
+      # 0 will be coerced to FALSE
+      # 1+ will be coerced to TRUE
+      vb$doPlot <- input$doPaisaje
+      })
+
     den_pp <- reactive({
-      den_pp <- switch(input$density_pp,
-                       'baja' = 100,
-                       'media' = 1250,
-                       'alta' = 3000)
+      den_pp <- switch(input$density_pp, 'baja' = 100, 'media' = 1250, 'alta' = 3000)
       })
 
     colour_tree_density <- reactive({
       colour_tree_density <- switch(input$density_pp,
-                                    'baja' = '#a1d99b',
-                                    'media' = '#238b45',
-                                    'alta' = '#00441b')
+                                    'baja' = '#a1d99b', 'media' = '#238b45','alta' = '#00441b')
       })
 
     rasterIni <- reactive({
-      d <- createLandscape(r,
-                           size_pp = input$size_pp,
-                           size_nf = input$size_nf,
-                           n_nf = input$n_nf)
+      d <- createLandscape(r, size_pp = input$size_pp, size_nf = input$size_nf, n_nf = input$n_nf)
       })
 
     output$medium_bird <- renderUI({
@@ -57,84 +58,69 @@ shinyServer(
 
     tableDisp <- reactive({
       myvals <- c(input$small_bird, input$medium_bird, 100-input$small_bird-input$medium_bird)
-      tabladispersantes <- data.frame(Names=c("Aves pequeño tamaño", "Aves mediano tamaño", "Mamíferos"),
-                 Values=myvals)
+      tabladispersantes <- data.frame(Dispersor=c("Small birds", "Meidum Birds", "Mammals"),
+                 Porcentaje=myvals)
       })
 
     animales <- reactive({
       myvals <- c(input$small_bird, input$medium_bird, 100-input$small_bird-input$medium_bird)
     })
 
-
     output$restable <- renderTable({
       tableDisp()
     })
 
-
     rasterRich <- reactive({
-
-      pastUse <- switch(input$pp_pastUse,
-                        'Bosque natural' = 'Oak',
-                        'Matorral' = 'Shrubland',
-                        'Pastizal' = 'Pasture',
-                        'Cultivo' = 'Crop')
-
+      pastUse <- switch(input$pp_pastUse, 'Bosque natural' = 'Oak', 'Matorral' = 'Shrubland',
+                        'Pastizal' = 'Pasture','Cultivo' = 'Crop')
       dist_raster <- dist2nf(rasterIni(), nf_value = 2)
 
-      myr_range <- as.data.frame(
-        cbind(value = c(0,1,2,3),
+      myr_range <- as.data.frame( cbind(value = c(0,1,2,3),
               lowRich = c(0, 12.82, mean(13.72, 15.62), 1),
               upRich = c(0, 13.34, mean(16.11, 19.66), 3)))
 
-      mapa_riqueza <- initRichness(r = rasterIni(),
-                                   draster = dist_raster,
-                                   r_range = myr_range,
-                                   treedensity = den_pp(), # density pine plantations
-                                   pastUse = pastUse,
-                                   rescale = FALSE)
-
+      mapa_riqueza <- initRichness(r = rasterIni(), draster = dist_raster,
+                                   r_range = myr_range, treedensity = den_pp(),
+                                   pastUse = pastUse, rescale = FALSE)
     })
 
     rasterDisp <- reactive({
-      v <- disper(x = rasterIni(),
-                  xr = rasterRich(),
-                  nf_value = 2,
-                  pp_value = 1)
+      v <- disper(x = rasterIni(), xr = rasterRich(), nf_value = 2, pp_value = 1)
       })
 
 
     output$initial_map <- renderPlot({
 
-      colores <- c('lightgoldenrod1', # Crops
-                   'green', # Natural forests
-                   'white', # Other
-                   colour_tree_density()) # Pine plantation
+      if (vb$doPlot == FALSE) return()
 
-      myKey <- list(text = list(lab = c("crop", "natural forest","other", "pine")),
-                    rectangles=list(col = colores),
-                    space='bottom', columns=4)
-
-      levelplot(rasterIni(), att='landuse', scales=list(draw=FALSE),
-                col.regions = colores, colorkey=FALSE, key = myKey)
-
-    })
+      isolate({
+        colores <- c('lightgoldenrod1', # Crops
+                     'green', # Natural forests
+                     'white', # Other
+                     colour_tree_density()) # Pine plantation
+        myKey <- list(text = list(lab = c("crop", "natural forest","other", "pine")),
+                      rectangles=list(col = colores), space='bottom', columns=4)
+        levelplot(rasterIni(), att='landuse', scales=list(draw=FALSE),
+                  col.regions = colores, colorkey=FALSE, key = myKey)
+        })
+      })
 
     output$richness_map <- renderPlot({
-
+      if (vb$doPlot == FALSE) return()
+      isolate({
       mapa_riqueza <- rasterRich()
-
       mapa_riqueza[mapa_riqueza == 0] <- NA
-
-      mytheme <- rasterTheme(region = brewer.pal(6, "YlGn"))
-
+      mytheme <- rasterTheme(region = brewer.pal(9, "YlGn"))
       levelplot(mapa_riqueza,
                 par.settings = mytheme, margin = FALSE,
                 scales=list(draw=FALSE),
                 colorkey = list(space = "bottom"),
                 pretty=TRUE)
+      })
     })
 
     output$richness_disper <- renderPlot({
+
       v <- rasterDisp()
 
       levelplot(v[['msb']],
@@ -167,8 +153,14 @@ shinyServer(
                         propaguleInputMammal = piM,
                         time_span = 1)
 
+      # pineLimit <- rasterToPolygons(rasterIni(), fun=function(x){x==1}, dissolve = TRUE)
+      mytheme <- rasterTheme(region = brewer.pal(9, "YlGn"))
+
       levelplot(stack(vv),
-                margin=FALSE,  par.settings = RdBuTheme)
+                par.settings = mytheme, margin = FALSE,
+                scales=list(draw=FALSE),
+                colorkey = list(space = "bottom"),
+                pretty=TRUE)
     })
 
 
