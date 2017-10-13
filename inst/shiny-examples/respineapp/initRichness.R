9754225e9d682b27ec00486c5ce756e29da60cf8
Antonio <ajpelu@gmail.com>
2017-10-13 11:57:31 +0200

    fix error richness

--- inst/shiny-examples/respineapp/initRichness.R
+++ inst/shiny-examples/respineapp/initRichness.R
@@ -31,54 +31,61 @@
 
 initRichness <- function(r, draster, r_range, treedensity, pastUse, rescale=TRUE){
 
-  # Natural forest (2)
-  aux <- r_range[which(r_range$value == 2), ]
-  r[r == 2]  <- sample(aux$lowRich:aux$upRich, ncell(r[r==2]), replace = TRUE)
+  # --- N cells
+  ncell_pp <- ncell(r[r == 1])
+  ncell_nf <- ncell(r[r == 2])
+  ncell_crop <- ncell(r[r == 3])
 
-  # Crops (3)
-  aux <- r_range[which(r_range$value == 3), ]
-  r[r == 3]  <- sample(aux$lowRich:aux$upRich, ncell(r[r==3]), replace = TRUE)
+  # --- Potential Richness values
+  ## Ranges
+  range_pp <- r_range[which(r_range$value == 1), ]
+  range_nf <- r_range[which(r_range$value == 2), ]
+  range_crop <- r_range[which(r_range$value == 3), ]
 
-  # Pine plantations (1)
-  # R ~ potR * (0.1*(pastUSE) + 0.7*(treeDensity))
-  ## ojo como asignamos los pesos??
-  # f <- (.1*fplu + .3*fdis + .6*ftreeden)
+  ## Potential vectors
+  potR_pp <- runif(ncell_pp*3, range_pp$lowRich, range_pp$upRich)
+  potR_nf <- runif(ncell_nf*3, range_nf$lowRich, range_nf$upRich)
+  potR_crop <- runif(ncell_crop*3, range_crop$lowRich, range_crop$upRich)
 
+  # --- Reclassify
+  r[r == 0] <- NA
+  r[r == 1] <- -100
+  r[r == 2] <- -200
+  r[r == 3] <- -300
 
+  # --- Pine plantation
   ## ~ TreeDensity
   ### Fraction of Potential Richness (tree Density Eq. 3 Gomez Aparicio et al. 2009)
   ftreeden <- exp(-0.5*((treedensity - 0.22)/1504.1)^2)
 
   ## ~ Distance to Seed Source
-
-  ## Compute diversity raster (See Gonzalez-Moreno et al. 2011)
+  ### Compute diversity raster (See Gonzalez-Moreno et al. 2011)
   sh <- calc(draster, fun=function(x){1.7605 - 0.0932*(sqrt(sqrt(x)))})
 
-  # Create a stack with the shanon diversity raster and landuse raster, and then compute values for pine plantations
-  s <- calc(stack(r, sh), fun=function(x)  ifelse(x[1] == 1 , x[1]*x[2],  NA))
+  ### Create a stack with the shanon diversity raster and landuse raster, and then compute values for pine plantations
+  s <- calc(stack(r, sh), fun=function(x)  ifelse(x[1] == -100 , (x[1]/-100)*x[2],  NA))
 
-  # Scale the distance effect from 0 to 1
+  ### Scale the distance effect from 0 to 1
   sh_scaled <- (s - cellStats(s, "min"))/(cellStats(s, "max") - cellStats(s, "min"))
 
   ## ~ PastUSE
   ### Past Land Use
   fplu <- ifelse(pastUse == 'Oak', .9999,
-               ifelse(pastUse == 'Shrubland', .4982,
-                      ifelse(pastUse == 'Crop', .0279, .0001)))
+                 ifelse(pastUse == 'Shrubland', .4982,
+                        ifelse(pastUse == 'Crop', .0279, .0001)))
 
-  ###### ---------------------------------------
-
-  ## Combine factor to correct pine plantations (OJO!!!!!!!!!!!!!! PESOS)
+  ## Combine factor to correct pine plantations
   f_pine <- (sh_scaled*0.35) + (.45*ftreeden + .2*fplu)
 
-  aux <- r_range[which(r_range$value == 1), ]
-  r[r == 1]  <- sample(aux$lowRich:aux$upRich, ncell(r[r==1]), replace = TRUE)
 
-  aux <- r_range[which(r_range$value == 3), ]
-  r[r == 3]  <- sample(aux$lowRich:aux$upRich, ncell(r[r==3]), replace = TRUE)
+  r[r == -100]  <- sample(potR_pp, ncell_pp, replace = TRUE)
+  r <- calc(stack(r, f_pine), fun = function(x) ifelse(x[1] < -100, x[1], x[1]*f_pine))
 
-  r <- calc(stack(r, f_pine),
-            fun = function(x) ifelse(x[1] == 1, x[1]*x[2], x[1]))
+  # --- Crops
+  r[r == -300]  <- sample(potR_crop, ncell_crop, replace = TRUE)
+
+  # --- Natural forest
+  r[r == -200]  <- sample(potR_nf, ncell_nf, replace = TRUE)
 
   # Rescale results
   if (rescale)
