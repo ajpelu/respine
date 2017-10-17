d03d14596aae301fe40e547badc8da891629429a
Antonio <ajpelu@gmail.com>
2017-10-17 16:45:33 +0200

    change layout

--- inst/shiny-examples/respineapp_ventana/ui.R
+++ inst/shiny-examples/respineapp_ventana/ui.R
@@ -20,10 +20,14 @@ sidebar <- dashboardSidebar(disable=TRUE)
   #          h5("Bosques naturales")
   #          )
 
+# Set heigth plots
+h_plots <- 1000
 
 body <- dashboardBody(
   fluidRow(
-    box(width = 2,
+    column(width = 4,
+
+    box(
       sliderInput(inputId = "size_pp", label = "Tamaño de la repoblación de pinar",
                     min = 200, max = 1500, value = 750),
       selectInput(inputId = "density_pp", label = "Densidad de la plantación",
@@ -37,41 +41,44 @@ body <- dashboardBody(
                   min = 50, max = 500, value = 250),
       actionButton("doPaisaje", "Configura Paisaje")
       ),
-    box(width = 2,
-        br(),br(),
-        h5('Dispersantes'),
-        sliderInput(inputId = "sb",label = "Aves pequeño tamaño",
+    box(
+      h5('Dispersantes'),
+      sliderInput(inputId = "sb",label = "Aves pequeño tamaño",
                     min = 0, max = 100, value = 0, step = 1),
-        uiOutput("mb"),
-        tableOutput("disptable"),
-        sliderInput("timeRange", "Número de años:", min=10, max=50, value=30),
-        actionButton("doRiqueza", "Calcula Riqueza")),
-    tabBox(width=8, side = "right",
+      uiOutput("mb"),
+      tableOutput("disptable"),
+      sliderInput("timeRange", "Número de años:", min=10, max=50, value=30),
+      actionButton("doRiqueza", "Calcula Riqueza")
+      )
+    ),
+
+    column(width = 8,
+    tabBox(side = "right",
            tabPanel("Paisaje Inicial", value = 'panel2',
                          withSpinner(
-                           plotOutput(outputId = 'initial_map'),
+                           plotOutput(outputId = 'initial_map', height = h_plots),
                            type=5, size=.8)),
                 tabPanel("Mapa de Riqueza Inicial", value = 'panel3',
                          withSpinner(
-                           plotOutput(outputId = 'richness_map'),
+                           plotOutput(outputId = 'richness_map', height = h_plots),
                            type=5, size = .8),
                          br(),br(),
                          h4('Riqueza Inicial de Especies'),
                          tableOutput("rich_table_init")),
                 tabPanel("Input Propágulos", value = 'panel4',
                          withSpinner(
-                           plotOutput(outputId = 'richness_disper'),
+                           plotOutput(outputId = 'richness_disper', height = h_plots),
                            type=5, size = .8)),
                 tabPanel("Mapa de Riqueza Tiempo", value = 'panel5',
                          withSpinner(
-                           plotOutput(outputId = 'richness_disperTime'),
+                           plotOutput(outputId = 'richness_disperTime', height = h_plots),
                            type=5, size = .8),
                          br(),br(),
                          h4('Riqueza Final de Especies'),
                          tableOutput("rich_table_end"))
+              ))
               )
-              )
-    )
+  )
 
 
 
