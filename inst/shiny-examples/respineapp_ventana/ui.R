d7486b36d8b2e85f0e37b044418ff05707b2b72f
Antonio <ajpelu@gmail.com>
2017-10-18 11:05:44 +0200

    grid layout

--- inst/shiny-examples/respineapp_ventana/ui.R
+++ inst/shiny-examples/respineapp_ventana/ui.R
@@ -26,35 +26,37 @@ h_plots <- 1000
 body <- dashboardBody(
   fluidRow(
     column(width = 4,
-
-    box(
-      sliderInput(inputId = "size_pp", label = "Tamaño de la repoblación de pinar",
-                    min = 200, max = 1500, value = 750),
-      selectInput(inputId = "density_pp", label = "Densidad de la plantación",
-                  choices = c('baja', 'media', 'alta'), selected = 'media'),
-      selectInput(inputId = "pp_pastUse", label = "Uso del pasado",
-                  choices = c('Bosque natural', 'Matorral', 'Pastizal', 'Cultivo'),
-                  selected = 'Matorral'),
-      sliderInput(inputId = "n_nf",label = "Nº bosques naturales",
-                  min = 1, max= 5, value =2),
-      sliderInput(inputId = "size_nf", label = "Tamaño",
-                  min = 50, max = 500, value = 250),
-      actionButton("doPaisaje", "Configura Paisaje")
+      fluidRow(
+        box(
+          h4("Pinar de Repoblación"),
+          sliderInput(inputId = "size_pp", label = "Tamaño de la repoblación de pinar",
+                      min = 200, max = 1500, value = 750),
+          selectInput(inputId = "density_pp", label = "Densidad de la plantación",
+                      choices = c('baja', 'media', 'alta'), selected = 'media'),
+          selectInput(inputId = "pp_pastUse", label = "Uso del pasado",
+                      choices = c('Bosque natural', 'Matorral', 'Pastizal', 'Cultivo'), selected = 'Matorral'),
+          sliderInput(inputId = "n_nf",label = "Nº bosques naturales", min = 1, max= 5, value =2),
+          sliderInput(inputId = "size_nf", label = "Tamaño", min = 50, max = 500, value = 250),
+          actionButton("doPaisaje", "Configura Paisaje")
+          ),
+        box(h4('Dispersantes'),
+          sliderInput(inputId = "sb",label = "Aves pequeño tamaño",
+                      min = 0, max = 100, value = 0, step = 1),
+          uiOutput("mb"),
+          tableOutput("disptable"),
+          sliderInput("timeRange", "Número de años:", min=10, max=50, value=30),
+          actionButton("doRiqueza", "Calcula Riqueza")
+        )
       ),
-    box(
-      h5('Dispersantes'),
-      sliderInput(inputId = "sb",label = "Aves pequeño tamaño",
-                    min = 0, max = 100, value = 0, step = 1),
-      uiOutput("mb"),
-      tableOutput("disptable"),
-      sliderInput("timeRange", "Número de años:", min=10, max=50, value=30),
-      actionButton("doRiqueza", "Calcula Riqueza")
-      )
-    ),
 
+      fluidRow(
+        box(title = "Title 1", width = 6, solidHeader = TRUE, "Box content 1"),
+        box(title = "Title 2", width = 6, solidHeader = TRUE, "Box content 2")
+        )
+      ),
     column(width = 8,
-    tabBox(side = "right",
-           tabPanel("Paisaje Inicial", value = 'panel2',
+      tabBox(width = NULL, side = "right",
+             tabPanel("Paisaje Inicial", value = 'panel2',
                          withSpinner(
                            plotOutput(outputId = 'initial_map', height = h_plots),
                            type=5, size=.8)),
@@ -75,10 +77,12 @@ body <- dashboardBody(
                            type=5, size = .8),
                          br(),br(),
                          h4('Riqueza Final de Especies'),
-                         tableOutput("rich_table_end"))
-              ))
-              )
-  )
+                         tableOutput("rich_table_end"))))
+  ))
+
+
+
+
 
 
 
